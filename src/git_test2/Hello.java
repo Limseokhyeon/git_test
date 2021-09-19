08ed6c4972f3ee6998214ee9973f62fbe916d3d9
Lim, seokhyeon <selver44444@gmail.com>
2021-09-20 07:35:03 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,10 +1,20 @@
 package git_test2;
 
-public class Hello { 
-	String message ="¾È³çÇÏ¼¼¿ä";
-	
-	public void say() 
-	{ System.out.println(message); 
-	
-	}
+public class Hello {
+
+    String message;
+
+    public Hello() {
+        this("�ȳ��ϼ���");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
+
+    public void say() {
+        System.out.println(message);
+    }
+
 }
+
