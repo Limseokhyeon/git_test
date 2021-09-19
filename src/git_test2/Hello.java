42409bb908fef0930647a7f825763e74fd65ac5c
Lim, seokhyeon <selver44444@gmail.com>
2021-09-20 07:38:27 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,10 +1,12 @@
 package git_test2;
 
-public class Hello { 
-	String message ="¾È³çÇÏ¼¼¿ä";
-	
-	public void say() 
-	{ System.out.println(message); 
-	
-	}
+public class Hello {
+    
+    String message = "안녕하세요";
+
+    public String say() {
+        return message;
+    }
+
 }
+
